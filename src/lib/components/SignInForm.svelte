59f324ec3a69f0bb52e45041c2961cc4c8cc3278
NotRewd <48103943+NotRewd@users.noreply.github.com>
2024-07-14 23:59:18 +0200

    Integrated appwrite authentication

--- src/lib/components/SignInForm.svelte
+++ src/lib/components/SignInForm.svelte
@@ -1,10 +1,44 @@
 <script>
+	import Icon from '@iconify/svelte';
+	import { account, ID } from '$lib/appwrite';
+
+	let busy = false;
+	let errorText = '';
+
+	async function loginUser(email, password) {
+		busy = true;
+
+		try {
+			await account.createEmailPasswordSession(email, password);
+		} catch (error) {
+			errorText = error.message;
+		}
+
+		busy = false;
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+
+		const form = event.target;
+		const formData = new FormData(form);
+		const email = formData.get('email');
+		const password = formData.get('password');
+
+		loginUser(email, password);
+	}
 </script>
 
-<form class="flex flex-col gap-5">
+<form on:submit={handleSubmit} class="flex max-w-md flex-col gap-5">
 	<label class="label">
-		<span>Username</span>
-		<input class="input px-3 py-2" type="text" name="text" placeholder="johndoe" required />
+		<span>Email</span>
+		<input
+			class="input px-3 py-2"
+			type="email"
+			name="text"
+			placeholder="john@example.com"
+			required
+		/>
 	</label>
 
 	<label class="label">
@@ -17,5 +51,17 @@
 			required
 		/>
 	</label>
-	<button type="submit" class="variant-filled-primary btn mt-5 self-start">Sign In</button>
+
+	{#if errorText}
+		<p class="max-w-xs text-red-500">{errorText}</p>
+	{/if}
+
+	{#if busy}
+		<div class="variant-ghost-primary btn mt-5 self-start">
+			<p>Signing in...</p>
+			<Icon icon="line-md:loading-loop" />
+		</div>
+	{:else}
+		<button type="submit" class="variant-filled-primary btn mt-5 self-start">Sign In</button>
+	{/if}
 </form>
